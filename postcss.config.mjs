397947f06b07bd4269285c6d5a8e9fec4c1cee1d
Kozyr Vsevolod <vsevolod.kozyr@nure.ua>
2026-09-13 21:06:19 +0300

    feat: colors done btw

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,12 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
+    "postcss-pxtorem": {
+      rootValue: 16,
+      propList: ["*"],
+      selectorBlackList: [],
+      minPixelValue: 1,
+    },
   },
 };
 

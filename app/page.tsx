b6c0f4910dadd4eb4ce7fe38fb0bffb10735477f
Kozyr Vsevolod <vsevolod.kozyr@nure.ua>
2026-09-13 23:48:15 +0300

    feat: btn size

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,73 @@
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        {/* Primary */}
+        <div className="flex gap-3">
+          <div className="flex flex-col gap-2">
+            <Button size={"default"}>Button</Button>
+            <Button size="default" disabled>
+              Button
+            </Button>
+            <Button size={"m"}>Button</Button>
+            <Button size="m" disabled>
+              Button
+            </Button>
+            <Button size={"xl"}>Button</Button>
+            <Button size="xl" disabled>
+              Button
+            </Button>
+            <Button size={"xl-2"}>Button</Button>
+            <Button size="xl-2" disabled>
+              Button
+            </Button>
+            <Button size={"xl-3"}>Button</Button>
+            <Button size="xl-3" disabled>
+              Button
+            </Button>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Button size={"icon"}>
+              <Search />
+            </Button>
+            <Button size="icon" disabled>
+              <Search />
+            </Button>
+            <Button size={"icon-m"}>
+              <Search />
+            </Button>
+            <Button size="icon-m" disabled>
+              <Search />
+            </Button>
+            <Button size={"icon-xl"}>
+              <Search />
+            </Button>
+            <Button size="icon-xl" disabled>
+              <Search />
+            </Button>
+            <Button size={"icon-xl-2"}>
+              <Search />
+            </Button>
+            <Button size="icon-xl-2" disabled>
+              <Search />
+            </Button>
+            <Button size={"icon-xl-3"}>
+              <Search />
+            </Button>
+            <Button size="icon-xl-3" disabled>
+              <Search />
+            </Button>
+          </div>
+        </div>
+        {/* Outline */}
+
+        {/* Text */}
+
+        {/* Icon size (primary, outline, text only) */}
+      </main>
     </div>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,46 +1,52 @@
-import { Button as ButtonPrimitive } from "@base-ui/react/button"
-import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "cn"
+import { Button as ButtonPrimitive } from "@base-ui/react/button";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "cn";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const buttonVariants = cva(
-  "group/button inline-flex shrink-0 items-center justify-center rounded-none border border-transparent bg-clip-padding text-xs font-semibold tracking-widest whitespace-nowrap uppercase transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-2 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5",
+  "font-inter group/button inline-flex shrink-0 items-center justify-center rounded-none border border-transparent bg-clip-padding tracking-widest whitespace-nowrap transition-all outline-none select-none focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 active:not-aria-[haspopup]:translate-y-px disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-2 aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        primary:
+          "bg-primary-500 text-primary-foreground rounded-[4px] hover:bg-primary-600 active:bg-primary-300 disabled:bg-neutral-400 disabled:text-neutral-600",
         outline:
           "border-border bg-transparent hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-input/30",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
-        ghost:
-          "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
-        destructive:
-          "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
-        link: "text-primary underline underline-offset-4 hover:underline",
+        text: "",
       },
       size: {
-        default:
-          "h-10 gap-1.5 px-6 has-data-[icon=inline-end]:pr-4 has-data-[icon=inline-start]:pl-4",
-        xs: "h-7 gap-1 px-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-9 gap-1 px-4 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
-        lg: "h-11 gap-1.5 px-8 has-data-[icon=inline-end]:pr-5 has-data-[icon=inline-start]:pl-5",
-        icon: "size-10",
-        "icon-xs": "size-7 [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-9",
-        "icon-lg": "size-11",
+        default: "h-[28px] gap-2 px-4 typo-button-xs",
+        m: "h-[32px] gap-2 px-4 typo-button-m",
+        xl: "h-[40px] gap-2 px-4 typo-button-xl",
+        "xl-2": "h-[48px] gap-2 px-4 typo-button-xl",
+        "xl-3": "h-[56px] gap-2 px-4 typo-button-xl",
+        //
+        icon: "size-[28px] [&_svg:not([class*='size-'])]:size-4",
+        "icon-m": "size-[32px] [&_svg:not([class*='size-'])]:size-5",
+        "icon-xl": "size-[40px] [&_svg:not([class*='size-'])]:size-6",
+        "icon-xl-2": "size-[48px] [&_svg:not([class*='size-'])]:size-6",
+        "icon-xl-3": "size-[56px] [&_svg:not([class*='size-'])]:size-6",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
-  }
-)
+  },
+);
+
+const arrowStyle = {
+  default: "size-[20px]",
+  xl: "size-[24px]",
+  "xl-2": "size-[24px]",
+  "xl-3": "size-[24px]",
+};
 
 function Button({
   className,
-  variant = "default",
+  variant = "primary",
   size = "default",
+  children,
   ...props
 }: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
   return (
@@ -48,8 +54,16 @@ function Button({
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
-  )
+    >
+      {!size?.includes("icon") && (
+        <ChevronLeft className={arrowStyle[size] || arrowStyle["default"]} />
+      )}
+      {children}
+      {!size?.includes("icon") && (
+        <ChevronRight className={arrowStyle[size] || arrowStyle["default"]} />
+      )}
+    </ButtonPrimitive>
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
